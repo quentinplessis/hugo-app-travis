9bcc8d3c8b222205de3d69ef5d0236d8f03199bb
Quentin Plessis <quentin@sorabito.com>
2017-09-21 09:06:49 +0900

    Modify blue green switch script.

--- kubernetes/blue-green.sh
+++ kubernetes/blue-green.sh
@@ -1,7 +1,9 @@
 
 export K8S_APP_NAME="hugo-app"
 export K8S_SERVICE_NAME="hugo-app-svc"
-export PROD_COLOR=`kubectl get svc ${K8S_SERVICE_NAME} --output=jsonpath={.spec.selector.color}`
-export TARGET_COLOR=`if [ "$PROD_COLOR" == "blue" ]; then echo "green"; else echo "blue"; fi`
+export K8S_TEMP_SERVICE_NAME="${K8S_SERVICE_NAME}-temp"
+export OLD_COLOR=`kubectl get svc ${K8S_SERVICE_NAME} --output=jsonpath={.spec.selector.color}`
+export NEW_COLOR=`if [ "$OLD_COLOR" == "blue" ]; then echo "green"; else echo "blue"; fi`
 
-kubectl set selector svc/${K8S_SERVICE_NAME} app=${K8S_APP_NAME},color=${TARGET_COLOR}
+kubectl set selector svc/${K8S_SERVICE_NAME} app=${K8S_APP_NAME},color=${NEW_COLOR}
+kubectl set selector svc/${K8S_TEMP_SERVICE_NAME} app=${K8S_APP_NAME},color=${OLD_COLOR}
